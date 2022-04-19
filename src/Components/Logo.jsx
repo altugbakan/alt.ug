ab9d22856dc665185de8dde7eca546e4e65e38f5
altugbakan <altugbakan@gmail.com>
2022-04-19 22:56:18 +0300

    fix: variable tailwindcss classes

--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -1,7 +1,7 @@
 const Logo = (props) => {
     return (
         <a href={props.href} target="_blank" rel="noreferrer" className="ml-4 mt-2">
-            <svg className={`fill-zinc-400 hover:fill-${props.color} hover:scale-125 ${props.extra}`} viewBox={props.viewBox} height={props.size} width={props.size}>
+            <svg className={`fill-zinc-400 hover:scale-125 ${props.imageStyle}`} viewBox={props.viewBox} height={props.size} width={props.size}>
                 <path d={props.path} />
             </svg>
         </a>

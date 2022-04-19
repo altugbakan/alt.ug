9902f1d8e5f1565af96f6aab3f0a2f067f1757b6
altugbakan <altugbakan@gmail.com>
2022-04-19 22:50:02 +0300

    feat: change max screen size

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import Projects from "./Components/Projects"
 
 export default function App() {
   return (
-    <div className="h-screen">
-      <NavBar />
-      <div className="flex flex-col items-center">
-        <h1 className="text-3xl font-bold text-white mb-2 lg:mb-12">
-          Altuğ's Personal Page
-        </h1>
-        <Projects />
-        <footer className="text-zinc-400 fixed bottom-0">contact me at <a href="mailto:mail@alt.ug" className="hover:underline">mail@alt.ug</a></footer>
+    <div className="flex justify-center">
+      <div className="h-screen max-w-screen-xl">
+        <NavBar />
+        <div className="flex flex-col items-center">
+          <h1 className="text-3xl font-bold text-white mb-6 lg:mb-12">
+            Altuğ's Personal Page
+          </h1>
+          <Projects />
+          <footer className="text-zinc-400 fixed bottom-0">contact me at <a href="mailto:mail@alt.ug" className="hover:underline">mail@alt.ug</a></footer>
+        </div>
       </div>
     </div>
   )

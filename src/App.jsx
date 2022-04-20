be100aa3b58fb0363e127489b99f28cd223608ef
altugbakan <altugbakan@gmail.com>
2022-04-20 20:23:53 +0300

    fix: support footer on small screens

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,16 @@ import Projects from "./Components/Projects"
 
 export default function App() {
   return (
-    <div className="flex justify-center">
-      <div className="h-screen max-w-screen-xl">
+    <div className="flex flex-row justify-center">
+      <div className="flex flex-col ml-2 mr-2 h-screen max-w-screen-xl">
         <NavBar />
         <div className="flex flex-col items-center">
-          <h1 className="text-3xl font-bold text-white mb-6 lg:mb-12">
+          <h1 className="text-3xl font-bold text-white mb-6 lg:mb-12 text-center">
             Altuğ's Personal Page
           </h1>
           <Projects />
-          <footer className="text-zinc-400 fixed bottom-0">contact me at <a href="mailto:mail@alt.ug" className="hover:underline">mail@alt.ug</a></footer>
         </div>
+        <footer className="text-zinc-400 mt-auto ml-auto mr-auto">contact me at <a href="mailto:mail@alt.ug" className="hover:underline">mail@alt.ug</a></footer>
       </div>
     </div>
   )

4d098b80d5dec656291ef171af406ad2a2738bc9
altugbakan <altugbakan@gmail.com>
2022-04-20 20:23:26 +0300

    change: list margin

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,27 +1,28 @@
 import Project from "./Project";
 
 const Projects = () => {
+    const marginBottom = "mb-4";
     return (
         <div>
             <h2 className="text-white text-2xl font-bold mb-4">Projects:</h2>
-            <ul className="text-white list-disc">
-                <li className="mb-2">
+            <ul className="text-white lg:list-disc">
+                <li className={marginBottom}>
                     <Project href="https://github.com/altugbakan/arduino-spotify-controller" title="Arduino Spotify Controller"
                         text="Allows you to control your Spotify player using Arduino Oplà IoT Kit and Spotify Web API." />
                 </li>
-                <li className="mb-2">
-                    <Project href="https://github.com/theloanstream/thels" title="The Loan Stream"
+                <li className={marginBottom}>
+                    <Project href="https://thels.vercel.app" title="The Loan Stream"
                         text="DeFi protocol that allows you to borrow Superfluid money streams without selling your collateral." />
                 </li>
-                <li className="mb-2">
+                <li className={marginBottom}>
                     <Project href="https://cryptogum.shop" title="Crypto Gums"
                         text="Personalized generative Gum NFTs on Polygon blockchain." />
                 </li>
-                <li className="mb-2">
+                <li className={marginBottom}>
                     <Project href="https://github.com/altugbakan/stellar-quest-go" title="Stellar Quest Go"
                         text="Solutions for Stellar Quest in Go using the Stellar Go SDK." />
                 </li>
-                <li className="mb-2">
+                <li className={marginBottom}>
                     <Project href="https://vanitymonkeygenerator.gitbook.io/vanity-monkey-generator/" title="Vanity MonKey Generator"
                         text="Generates an account with a custom MonKey for Banano cryptocurrency." />
                 </li>

14716e95c858f2ba573250b452ff972f0bc9fa2f
altugbakan <altugbakan@gmail.com>
2022-04-24 19:12:35 +0300

    feat: new project display

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,31 +1,30 @@
-import Project from "./Project";
+import ProjectData from "../utils/ProjectData";
+
+const Link = <svg xmlns="http://www.w3.org/2000/svg" className="fill-zinc-400 hover:fill-white" width="24" height="24" viewBox="0 0 24 24"><path d="M6.188 8.719c.439-.439.926-.801 1.444-1.087 2.887-1.591 6.589-.745 8.445 2.069l-2.246 2.245c-.644-1.469-2.243-2.305-3.834-1.949-.599.134-1.168.433-1.633.898l-4.304 4.306c-1.307 1.307-1.307 3.433 0 4.74 1.307 1.307 3.433 1.307 4.74 0l1.327-1.327c1.207.479 2.501.67 3.779.575l-2.929 2.929c-2.511 2.511-6.582 2.511-9.093 0s-2.511-6.582 0-9.093l4.304-4.306zm6.836-6.836l-2.929 2.929c1.277-.096 2.572.096 3.779.574l1.326-1.326c1.307-1.307 3.433-1.307 4.74 0 1.307 1.307 1.307 3.433 0 4.74l-4.305 4.305c-1.311 1.311-3.44 1.3-4.74 0-.303-.303-.564-.68-.727-1.051l-2.246 2.245c.236.358.481.667.796.982.812.812 1.846 1.417 3.036 1.704 1.542.371 3.194.166 4.613-.617.518-.286 1.005-.648 1.444-1.087l4.304-4.305c2.512-2.511 2.512-6.582.001-9.093-2.511-2.51-6.581-2.51-9.092 0z" /></svg>;
+const GitHub = <svg xmlns="http://www.w3.org/2000/svg" className="fill-zinc-400 hover:fill-white" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" /></svg>;
 
 const Projects = (props) => {
-    const marginBottom = "mb-4";
     return (
         <div className={props.className}>
-            <h2 className="text-white text-2xl font-bold mb-4">Projects:</h2>
-            <ul className="text-white lg:list-disc">
-                <li className={marginBottom}>
-                    <Project href="https://github.com/altugbakan/arduino-spotify-controller" title="Arduino Spotify Controller"
-                        text="Allows you to control your Spotify player using Arduino Oplà IoT Kit and Spotify Web API." />
-                </li>
-                <li className={marginBottom}>
-                    <Project href="https://thels.vercel.app" title="The Loan Stream"
-                        text="DeFi protocol that allows you to borrow Superfluid money streams without selling your collateral." />
-                </li>
-                <li className={marginBottom}>
-                    <Project href="https://cryptogum.shop" title="Crypto Gums"
-                        text="Personalized generative Gum NFTs on Polygon blockchain." />
-                </li>
-                <li className={marginBottom}>
-                    <Project href="https://github.com/altugbakan/stellar-quest-go" title="Stellar Quest Go"
-                        text="Solutions for Stellar Quest in Go using the Stellar Go SDK." />
-                </li>
-                <li className={marginBottom}>
-                    <Project href="https://vanitymonkeygenerator.gitbook.io/vanity-monkey-generator/" title="Vanity MonKey Generator"
-                        text="Generates an account with a custom MonKey for Banano cryptocurrency." />
-                </li>
+            <h2 className="text-white text-2xl font-bold mb-4">Projects</h2>
+            <ul className="text-white">
+                {
+                    ProjectData.map((project) => {
+                        return (
+                            <li key={project.name} className="flex flex-row even:flex-row-reverse text-center justify-between gap-2 items-center mb-16">
+                                <img src={project.img} alt={project.name} className="w-1/2 h-1/2" />
+                                <div>
+                                    <h2 className="font-bold text-xl">{project.name}</h2>
+                                    <p>{project.description}</p>
+                                    <div className="flex flex-row justify-around mt-2">
+                                        <a href={project.href} target="_blank" rel="noreferrer">{Link}</a>
+                                        <a href={project.github} target="_blank" rel="noreferrer">{GitHub}</a>
+                                    </div>
+                                </div>
+                            </li>
+                        );
+                    })
+                }
             </ul>
         </div>
     );
---
+++ src/utils/ProjectData.js
@@ -0,0 +1,35 @@
+import ArduinoSpotifyController from "../images/arduinospotifycontroller.png";
+import Gums from "../images/gums.gif";
+import Thels from "../images/thels.png";
+import StellarQuestGo from "../images/stellarquestgo.png";
+import VanityMonKeyGenerator from "../images/vanitymonkeygenerator.png";
+
+const ProjectData = [
+    {
+        name: "Arduino Spotify Controller", href: "https://create.arduino.cc/projecthub/Altug/opla-spotify-controller-6e7bc4",
+        img: ArduinoSpotifyController, description: "Allows you to control your Spotify player using Arduino Oplà IoT Kit and Spotify Web API.",
+        github: "https://github.com/altugbakan/arduino-spotify-controller",
+    },
+    {
+        name: "The Loan Stream", href: "https://thels.vercel.app/",
+        img: Thels, description: "A novel DeFi protocol that allows you to borrow money streams without selling your collateral.",
+        github: "https://github.com/theloanstream/thels",
+    },
+    {
+        name: "Crypto Gums", href: "https://cryptogum.shop/",
+        img: Gums, description: "Personalized generative Gum NFTs on Polygon blockchain.",
+        github: "https://github.com/altugbakan/crypto-gums",
+    },
+    {
+        name: "Stellar Quest Go", href: "https://quest.stellar.org/",
+        img: StellarQuestGo, description: "Solutions for Stellar Quest in Go using the Stellar Go SDK.",
+        github: "https://github.com/altugbakan/stellar-quest-go",
+    },
+    {
+        name: "Vanity MonKey Generator", href: "https://vanitymonkeygenerator.gitbook.io/",
+        img: VanityMonKeyGenerator, description: "Generates an account with a custom MonKey for Banano cryptocurrency.",
+        github: "https://github.com/altugbakan/vanity-monkey-generator",
+    }
+];
+
+export default ProjectData;

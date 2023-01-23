7f447bcc1980253166860b9a8ea399c9bb8a5f89
altugbakan <altugbakan@gmail.com>
2023-01-23 19:51:13 +0300

    feat: add SoroDogs project

--- src/utils/ProjectData.js
+++ src/utils/ProjectData.js
@@ -5,43 +5,70 @@ import StellarQuestGo from "../images/stellarquestgo.png";
 import VanityMonKeyGenerator from "../images/vanitymonkeygenerator.png";
 import CryptoRewardCards from "../images/cryptorewardcards.png";
 import Vapor from "../images/vapor.png";
+import SoroDogs from "../images/sorodogs.png";
 
 const ProjectData = [
-    {
-        name: "Vapor", href: "https://vapor-app.vercel.app",
-        img: Vapor, description: "Allows people to create no-gas trade offers between tokens and NFTs.",
-        github: "https://github.com/vapor-swap/vapor-contracts",
-    },
-    {
-        name: "Crypto Reward Cards", href: "https://reward-cards.surge.sh",
-        img: CryptoRewardCards, description: "A way for business owners to reward their users with automated token rewards and membership tiers.",
-        github: "https://github.com/reward-cards/reward-cards",
-    },
-    {
-        name: "Arduino Spotify Controller", href: "https://create.arduino.cc/projecthub/Altug/opla-spotify-controller-6e7bc4",
-        img: ArduinoSpotifyController, description: "Allows you to control your Spotify player using Arduino Oplà IoT Kit and Spotify Web API.",
-        github: "https://github.com/altugbakan/arduino-spotify-controller",
-    },
-    {
-        name: "The Loan Stream", href: "https://thels.vercel.app/",
-        img: Thels, description: "A novel DeFi protocol that allows you to borrow money streams without selling your collateral.",
-        github: "https://github.com/theloanstream/thels",
-    },
-    {
-        name: "Crypto Gums", href: "https://gums.surge.sh/",
-        img: Gums, description: "Personalized generative Gum NFTs on Polygon blockchain.",
-        github: "https://github.com/altugbakan/crypto-gums",
-    },
-    {
-        name: "Stellar Quest Go", href: "https://quest.stellar.org/",
-        img: StellarQuestGo, description: "Solutions for Stellar Quest in Go using the Stellar Go SDK.",
-        github: "https://github.com/altugbakan/stellar-quest-go",
-    },
-    {
-        name: "Vanity MonKey Generator", href: "https://vanitymonkeygenerator.gitbook.io/",
-        img: VanityMonKeyGenerator, description: "Generates an account with a custom MonKey for Banano cryptocurrency.",
-        github: "https://github.com/altugbakan/vanity-monkey-generator",
-    }
+  {
+    name: "SoroDogs",
+    href: "https://sorodogs.surge.sh",
+    img: SoroDogs,
+    description: "NFTs on Stellar using Soroban Platform",
+    github: "https://github.com/altugbakan/sorodogs",
+  },
+  {
+    name: "Vapor",
+    href: "https://vapor-app.vercel.app",
+    img: Vapor,
+    description:
+      "Allows people to create no-gas trade offers between tokens and NFTs.",
+    github: "https://github.com/altugbakan/vapor-app",
+  },
+  {
+    name: "Crypto Reward Cards",
+    href: "https://reward-cards.surge.sh",
+    img: CryptoRewardCards,
+    description:
+      "A way for business owners to reward their users with automated token rewards and membership tiers.",
+    github: "https://github.com/altugbakan/reward-cards",
+  },
+  {
+    name: "Arduino Spotify Controller",
+    href: "https://create.arduino.cc/projecthub/Altug/opla-spotify-controller-6e7bc4",
+    img: ArduinoSpotifyController,
+    description:
+      "Allows you to control your Spotify player using Arduino Oplà IoT Kit and Spotify Web API.",
+    github: "https://github.com/altugbakan/arduino-spotify-controller",
+  },
+  {
+    name: "The Loan Stream",
+    href: "https://thels.vercel.app/",
+    img: Thels,
+    description:
+      "A novel DeFi protocol that allows you to borrow money streams without selling your collateral.",
+    github: "https://github.com/theloanstream/thels",
+  },
+  {
+    name: "Crypto Gums",
+    href: "https://gums.surge.sh/",
+    img: Gums,
+    description: "Personalized generative Gum NFTs on Polygon blockchain.",
+    github: "https://github.com/altugbakan/crypto-gums",
+  },
+  {
+    name: "Stellar Quest Go",
+    href: "https://quest.stellar.org/",
+    img: StellarQuestGo,
+    description: "Solutions for Stellar Quest in Go using the Stellar Go SDK.",
+    github: "https://github.com/altugbakan/stellar-quest-go",
+  },
+  {
+    name: "Vanity MonKey Generator",
+    href: "https://vanitymonkeygenerator.gitbook.io/",
+    img: VanityMonKeyGenerator,
+    description:
+      "Generates an account with a custom MonKey for Banano cryptocurrency.",
+    github: "https://github.com/altugbakan/vanity-monkey-generator",
+  },
 ];
 
 export default ProjectData;

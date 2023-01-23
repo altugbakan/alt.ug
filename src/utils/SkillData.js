dd01f836ab76974cc03cda5bab285ba7655e82c8
altugbakan <altugbakan@gmail.com>
2023-01-23 19:51:26 +0300

    chore: update skills

--- src/utils/SkillData.js
+++ src/utils/SkillData.js
@@ -1,53 +1,39 @@
-import Arduino from "../images/arduino.png";
-import Brownie from "../images/brownie.png";
 import C from "../images/c.png";
 import Cpp from "../images/cpp.png";
 import CSharp from "../images/csharp.png";
-import Css from "../images/css.png";
-import Ethereum from "../images/ethereum.png";
-import Flask from "../images/flask.png";
-import Foundry from "../images/foundry.png";
-import Git from "../images/git.png";
-import GitHub from "../images/github.png";
-import Go from "../images/go.png";
-import Hardhat from "../images/hardhat.png";
-import Html from "../images/html.png";
 import Js from "../images/js.png";
-import Node from "../images/node.png";
+import Linux from "../images/linux.png";
 import Python from "../images/python.png";
-import PyTorch from "../images/pytorch.png";
-import Qt from "../images/qt.png";
 import React from "../images/react.png";
 import Solidity from "../images/solidity.png";
-import Tailwind from "../images/tailwind.png";
-import VisualStudio from "../images/visualstudio.png";
-import VsCode from "../images/vscode.png";
+import Ts from "../images/ts.png";
 
 const SkillData = [
-    { img: C, alt: "C", href: "https://www.iso.org/standard/74528.html/" },
-    { img: Cpp, alt: "C++", href: "https://isocpp.org/" },
-    { img: CSharp, alt: "C#", href: "https://docs.microsoft.com/en-us/dotnet/csharp/" },
-    { img: Go, alt: "Go", href: "https://go.dev/" },
-    { img: Python, alt: "Python", href: "https://www.python.org/" },
-    { img: Html, alt: "HTML", href: "https://html.spec.whatwg.org/" },
-    { img: Css, alt: "CSS", href: "https://www.w3.org/TR/CSS/#css/" },
-    { img: Tailwind, alt: "Tailwind CSS", href: "https://tailwindcss.com/" },
-    { img: Js, alt: "JavaScript", href: "https://www.ecma-international.org/publications-and-standards/standards/ecma-262/" },
-    { img: Node, alt: "Node.js", href: "https://nodejs.org/" },
-    { img: React, alt: "React", href: "https://reactjs.org/" },
-    { img: Ethereum, alt: "Ethereum", href: "https://ethereum.org/" },
-    { img: Solidity, alt: "Solidity", href: "https://soliditylang.org/" },
-    { img: Foundry, alt: "Foundry", href: "https://book.getfoundry.sh/ " },
-    { img: Brownie, alt: "Brownie", href: "https://eth-brownie.readthedocs.io/en/stable/" },
-    { img: Hardhat, alt: "Hardhat", href: "https://hardhat.org/" },
-    { img: Flask, alt: "Flask", href: "https://flask.palletsprojects.com/" },
-    { img: Git, alt: "Git", href: "https://git-scm.com/" },
-    { img: GitHub, alt: "GitHub", href: "https://github.com/" },
-    { img: Qt, alt: "Qt", href: "https://www.qt.io/" },
-    { img: VisualStudio, alt: "Visual Studio", href: "https://visualstudio.microsoft.com/" },
-    { img: VsCode, alt: "Visual Studio Code", href: "https://code.visualstudio.com/" },
-    { img: PyTorch, alt: "PyTorch", href: "https://pytorch.org/" },
-    { img: Arduino, alt: "Arduino", href: "https://arduino.cc/" },
+  { img: C, alt: "C", href: "https://www.iso.org/standard/74528.html/" },
+  { img: Cpp, alt: "C++", href: "https://isocpp.org/" },
+  {
+    img: CSharp,
+    alt: "C#",
+    href: "https://docs.microsoft.com/en-us/dotnet/csharp/",
+  },
+  { img: Linux, alt: "Linux", href: "https://kernel.org/" },
+  { img: Python, alt: "Python", href: "https://www.python.org/" },
+  {
+    img: Js,
+    alt: "JavaScript",
+    href: "https://www.ecma-international.org/publications-and-standards/standards/ecma-262/",
+  },
+  {
+    img: Ts,
+    alt: "TypeScript",
+    href: "https://www.typescriptlang.org/",
+  },
+  {
+    img: React,
+    alt: "React",
+    href: "https://reactjs.org/",
+  },
+  { img: Solidity, alt: "Solidity", href: "https://soliditylang.org/" },
 ];
 
 export default SkillData;

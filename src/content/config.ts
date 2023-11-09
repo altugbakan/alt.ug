ad8f667dbf41b4703d6b85ad775071eab41d1585
Altuğ Bakan <mail@alt.ug>
2023-11-09 23:54:36 +0100

    Add post collection and markdown styling

--- src/content/config.ts
+++ src/content/config.ts
@@ -11,6 +11,17 @@ const projectCollection = defineCollection({
   }),
 });
 
+const postCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    date: z.date(),
+    tags: z.array(z.string()),
+  }),
+});
+
 export const collections = {
   projects: projectCollection,
+  posts: postCollection,
 };

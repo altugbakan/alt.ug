d201ea1435a7d8612c00fba4d36d2bcc55c4d431
Altuğ Bakan <mail@alt.ug>
2024-01-28 15:27:26 +0100

    Add sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  site: "https://alt.ug",
+  integrations: [tailwind(), mdx(), sitemap()],
 });
